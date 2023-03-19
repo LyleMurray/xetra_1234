9eea5b3ff82ee393275571b73ac0436e8f4443cc
Lyle Murray <lmurray@campbellsupplyco.com>
2023-03-19 18:55:19 -0500

    WIP setting up classes

---
+++ xetra/common/meta_process.py
@@ -0,0 +1,14 @@
+"""Meta File processes"""
+
+class MetaProcess():
+    """
+    Class for interacting with the meta file
+    """
+    
+    @staticmethod
+    def update_meta_file():
+        pass
+
+    @staticmethod
+    def return_date_list():
+        pass
